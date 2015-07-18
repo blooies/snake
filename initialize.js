a0aedf0e6079de6f9447fc1319ee1ad95b1a964a
blooies <beverlymah@gmail.com>
2015-07-17 22:30:39 -0400

    more clean up

--- initialize.js
+++ initialize.js
@@ -1,3 +1,9 @@
+// classic game of snake. 
+// after every level, 
+// apple on board increases by one. 
+// and snakes length will increase. 
+// spacebar to play and pause.
+
 $(document).ready(function() {
     var snake = new Snake([[19,12],[19,13],[19,14],[19,15],[19,16],[19,17]], 'west'),
     	view = new GameView(),
